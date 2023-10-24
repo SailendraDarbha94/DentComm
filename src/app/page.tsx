ee9cf901fa5582d8797fb3023d0ca383711db6da
Sailendra Darbha <sailendra.darbha@gmail.com>
2023-10-24 18:59:28 +0530

    config done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
-  )
+    <MaxWidthWrapper className="mb-10 mt-24 sm:mt-40 flex flex-col items-center justify-center text-center">
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700">
+          SmartBag is now open for public beta!
+        </p>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,19 @@
+import { classNames } from '@/lib/utils'
+import { ReactNode } from 'react'
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: ReactNode
+}) => {
+  return (
+    <div className={classNames('mx-auto w-full max-w-screen-xl px-2 md:px-1', className)}>
+        {children}
+    </div>
+  )
+
+}
+
+export default MaxWidthWrapper
