c8c7adcca1af50576ccfb8fb5671e7fc85155ae6
Sailendra Darbha <sailendradarbha@Sailendras-MacBook-Air.local>
2024-02-07 12:50:33 +0530

    hello world blank slate

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-red-100 m-2 p-4">
+      Hello World
+    </main>
+  );
+}
